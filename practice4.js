7765eb10d1e51b86c6d6699709affb36cd205d8a
Jagadeesh Chenchali <jagadeeshch789@gmail.com>
2023-08-23 20:45:03 +0530

    modified practice4

--- practice4.js
+++ practice4.js
@@ -1,6 +1,16 @@
-function abbrev_name (string) 
+/*function abbrev_name (string) 
 {
     let split_string = string.trim().split(" ");
+    if (split_string.length > 1) 
+    {
+        return (split_string[0] + " " + split_string[1].charAt(0) + ".");
+    }
+    return split_string;
+};
+console.log(abbrev_name("Robin Singh"));*/
+function abbrev_name (string) 
+{
+    let split_string = string.split(" ");
     if (split_string.length > 1) 
     {
         return (split_string[0] + " " + split_string[1].charAt(0) + ".");
