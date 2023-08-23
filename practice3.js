e9d830bc7e2f1ba748f8f177fc05730c406e3241
Jagadeesh Chenchali <jagadeeshch789@gmail.com>
2023-08-23 20:11:12 +0530

    modified practice3

--- practice3.js
+++ practice3.js
@@ -1,9 +1,9 @@
 
-sliceString = function (string, length) 
+truncate_string = function (string, length) 
 {
     if ((string.constructor === String) && (length>0)) 
     {
         return string.slice(0, length);
     }
 };
-console.log(sliceString("Robin Singh",4));
+console.log(truncate_string("Robin Singh",4));
