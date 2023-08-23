8644cacf2fe804ca05ffc69e1b6c3eb59578b905
Jagadeesh Chenchali <jagadeeshch789@gmail.com>
2023-08-23 20:43:01 +0530

    added practice4.js

---
+++ practice4.js
@@ -0,0 +1,10 @@
+function abbrev_name (string) 
+{
+    let split_string = string.trim().split(" ");
+    if (split_string.length > 1) 
+    {
+        return (split_string[0] + " " + split_string[1].charAt(0) + ".");
+    }
+    return split_string;
+};
+console.log(abbrev_name("Robin Singh"));
