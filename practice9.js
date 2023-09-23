bcf169460ec66daabef7df4f2f2fc4ce0defc4c9
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-23 18:26:38 +0530

    modified

--- practice9.js
+++ practice9.js
@@ -7,8 +7,6 @@ console.log(protect_email("robin_singh@example.com"));
 
 function protect_email(email){
 const parts= email.split("@");
-const username= parts[0];           
-const hiddenUsername= username.slice(0,5)+"..."+username.slice(-2);
-return hiddenUsername+"@"+parts[1];
+return parts[0].slice(0,5)+"..."+parts[0].slice(-2)+"@"+parts[1];
 }
 console.log(protect_email("jagadeeshch789@gmail.com"));
