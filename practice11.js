b8d7a9ff94ff02f02d4352e5be6f4e8417375e99
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-23 19:17:03 +0530

    practice captilize first letter of every word of a statement

--- practice11.js
+++ practice11.js
@@ -5,7 +5,19 @@ console.log(capitalize('js string exercises'));
 "Js string exercises"
 */
 
-function captilize(string){
+/*function captilize(string){
     return string.charAt(0).toUpperCase()+string.slice(1);
 }
 console.log(captilize("js string exercises"));
+*/
+
+// Captrilize first letter of every word in the sentence.
+/*function captilize(string) {
+    const parts= string.split(" ");
+    return parts.maxLength;
+}*/
+
+const string= "js string exercises";
+const parts= string.split(" ");
+const count= parts.length;
+console.log(count);
