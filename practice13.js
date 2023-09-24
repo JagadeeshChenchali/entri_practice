d207e072ca55f3e02172ba2f726899d52028a0be
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-24 07:15:02 +0530

    checking leap year

---
+++ practice13.js
@@ -0,0 +1,16 @@
+/*
+9)Leap Year Check
+Write a function isLeapYear that takes a year as input and prints "Leap year" if the year is 
+divisible by 4 and not divisible by 100, or if it's divisible by 400. Otherwise, print "Not a leap 
+year."
+*/
+
+function isLeapYear(year){
+    if((year % 4===0 && year % 100 !==0) || (year%400===0)){
+        console.log("Leap Year");
+    }
+    else{
+        console.log("Not a Leap Year")
+    }
+}
+console.log(isLeapYear(2024));
