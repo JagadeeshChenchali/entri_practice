a92aa2f5a913f37cc88b3ee607789354dcecb45a
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-24 11:40:20 +0530

    array

---
+++ practice22.js
@@ -0,0 +1,18 @@
+/*
+Stack Operations
+Implement a stack using an array. Create a stack called `myStack` and perform the following 
+operations:
+- Push the values 10, 20, and 30 onto the stack.
+- Pop the top element from the stack.
+- Push the value 40 onto the stack.
+Print the stack after each operation.
+*/
+
+const myStack= [];
+console.log(myStack);
+myStack.push(10,20,30);
+console.log(myStack);
+myStack.pop();
+console.log(myStack);
+myStack.push(40);
+console.log(myStack);
