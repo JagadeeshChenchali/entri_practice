7dd207abcac07cd3b970456e14d217140a69139b
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-25 15:18:40 +0530

    reverse string

---
+++ practice29.js
@@ -0,0 +1,13 @@
+/*
+Reverse a String
+Write a program to reverse a string using a `for` loop
+*/
+
+function reverseString(string){
+    let reversed="";
+    for(let i=string.length-1;i>=0;i--){
+        reversed+=string[i];
+    }
+    return reversed;
+}
+console.log(reverseString("hello"));
