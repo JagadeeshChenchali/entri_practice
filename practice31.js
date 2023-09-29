d9ed92dd10fc1bf5038659af84234dd77eefe428
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-29 07:13:35 +0530

    practice

--- practice31.js
+++ practice31.js
@@ -4,8 +4,24 @@
 loop.
 */
 
-function count(n){
+/*function count(n){
     let s=n.length;
     console.log(s);
 }
-console.log(count("10"));
+console.log(count("10"));*/
+
+function digits_count(n) {
+  var count = 0;
+  if (n >= 1) ++count;
+
+  while (n / 10 >= 1) {
+    n /= 10;
+    ++count;
+  }
+
+  return count;
+}
+
+console.log(digits_count(12112));
+
+console.log(digits_count(457));
