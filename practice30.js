8a75209c9e1780e67139c938a2b08c5d252cf636
JagadeeshChenchali <jagadeeshch789@gmail.com>
2023-09-29 06:13:06 +0530

    practice

--- practice30.js
+++ practice30.js
@@ -2,8 +2,23 @@
 Prime Number Check
 Write a program to check if a given number `n` is prime using a `for` loop.*/
 
+function isPrime(n){
+   let count=0;
+   for (i=1;i<=n;i++){
+      if (n%i==0){
+         count+=1;
+      }
+   }
+   if (count==2){
+      console.log("Prime Number");
+   }
+   else{
+      console.log("NOt a prime number");
+   }
+}
+console.log(isPrime(15));
 
-function isPrime(number){
+/*function isPrime(number){
     let count = 0;
     for(let i = 1;i<=number;i++)    {
        if(number%i == 0){
@@ -17,4 +32,4 @@ function isPrime(number){
        return false;
     }
  }
- console.log(isPrime(10));
+ console.log(isPrime(10));*/
